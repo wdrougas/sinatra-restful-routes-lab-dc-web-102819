8651a1427c804e9ab7d73e52a9ceb244a07a4d33
Will Drougas <wdrougas@gmail.com>
2019-11-19 14:56:06 -0500

    Nov 19

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,27 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
+  post "/recipes" do
+    Recipe.create(:name=> params[:name], :ingredients=> params[:ingredients], :cook_time=> params[:cook_time])
+    binding.pry
+    erb :index
+  end
+
+  get "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get "/recipes/:id/edit" do
+    @recipe = Recipe.find(params[:id])
+    binding.pry
+    erb :edit
+  end
+
+  put "/recipes/:id" do
+    @recipe = Recipe.find(params[:id])
+    binding.pry
+    erb :show
+  end
+
 end
